5681c67b99ae7456401df02e0cb6bd36bee50cc1
jakubkalinski0 <kubakalinski2@gmail.com>
2025-03-12 22:59:30 +0100

    Cosmetic changes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 This script serves as the entry point for the Reversi game.
 
 It initializes the game by creating an instance of the `Game` class and starts the gameplay loop.
-The `Game` class is responsible for managing the overall flow of the game, including player turns, board state, and game logic.
+The `Game` class is responsible for managing the overall flow of the game, including player turns, board state, and game
+logic.
 """
 
 from Game import Game
